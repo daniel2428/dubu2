62c3b90881b4a21ee21c9f387bafca5d25ded3b0
daniel2428 <turatbekovdaniel2005@gmail.com>
2025-03-31 18:37:11 +0600

    hw6 and 7

--- db/queries.py
+++ db/queries.py
@@ -1,43 +1,49 @@
 CREATE_TABLE_TASKS = """
-
- 
-
     CREATE TABLE IF NOT EXISTS tasks (
- 
-
         id INTEGER PRIMARY KEY AUTOINCREMENT,
- 
-
-        task TEXT NOT NULL
- 
-
-        )
- 
-
+        title TEXT NOT NULL,
+        task TEXT NOT NULL,
+        created_at TEXT NOT NULL,
+        completed INTEGER DEFAULT 0
+    );
 """
- 
 
+SELECT_TASKS = """
+    SELECT id, title, task, created_at, completed FROM tasks;
+"""
 
- 
+INSERT_TASK = """
+    INSERT INTO tasks (title, task, created_at, completed) VALUES (?, ?, ?, 0);
+"""
 
-SELECT_TASKS = "SELECT id, task FROM tasks"
- 
+UPDATE_TASK = """
+    UPDATE tasks SET title = ?, task = ? WHERE id = ?;
+"""
 
+DELETE_TASK = """
+    DELETE FROM tasks WHERE id = ?;
+"""
 
- 
+SELECT_ALL = """
+    SELECT id, task, completed FROM tasks;
+"""
 
-INSERT_TASK = "INSERT INTO tasks (task) VALUES (?)"
- 
+SELECT_COMPLETED = """
+    SELECT id, task, completed FROM tasks WHERE completed = 2;
+"""
 
+SELECT_IN_WORK = """
+    SELECT id, task, completed FROM tasks WHERE completed = 1;
+"""
 
- 
+SELECT_INCOMPLETE = """
+    SELECT id, task, completed FROM tasks WHERE completed = 0;
+"""
 
-UPDATE_TASK = "UPDATE tasks SET task = ? WHERE id = ?"
- 
+UPDATE_STATUS = """
+    UPDATE tasks SET completed = ? WHERE id = ?;
+"""
 
-
- 
-
-DELETE_TASK = "DELETE FROM tasks WHERE id = ?"
- 
-
+DELETE_COMPLETED = """
+    DELETE FROM tasks WHERE completed = 2;
+"""
--- main.py
+++ main.py
@@ -1,212 +1,141 @@
 import flet as ft
-
- 
-
 from db import main_db
- 
 
-from datetime import datetime
- 
-
-
- 
 
 def main(page: ft.Page):
- 
-
-    page.title = 'Task Manager'
- 
-
-    page.padding = 75
- 
-
-    page.bgcolor = ft.Colors.DEEP_ORANGE_ACCENT
- 
-
+    page.title = 'Todo List'
     page.theme_mode = ft.ThemeMode.DARK
- 
-
-
- 
+    page.window_maximized = True
 
     task_list = ft.Column(spacing=10)
- 
-
-
- 
+    filter_type = "all"
 
     def load_tasks():
- 
-
         task_list.controls.clear()
- 
-
-        for task_id, task_text, created_at in main_db.get_tasks():
- 
-
-            task_list.controls.append(create_task_row(task_id, task_text, created_at))
- 
-
+        for task_id, task_text, completed in main_db.get_tasks(filter_type):
+            task_list.controls.append(create_task_row(task_id, task_text, completed))
         page.update()
- 
 
-
- 
-
-    def create_task_row(task_id, task_text, created_at):
- 
-
+    def create_task_row(task_id, task_text, completed):
         task_field = ft.TextField(value=task_text, expand=True, dense=True, read_only=True)
- 
+        in_work = ft.Checkbox(
+            label="В процессе",
+            value=(completed == 1),
+            tristate=False,
+            on_change=lambda e: toggle_in_work(task_id, e.control.value)
+        )
 
-        timestamp = ft.Text(created_at, color=ft.Colors.BLACK, size=12)
- 
-
-
- 
-
-
- 
-
+        task_done = ft.Checkbox(
+            label="Завершено",
+            value=(completed == 2),
+            tristate=False,
+            on_change=lambda e: toggle_task(task_id, e.control.value)
+        )
+        
         def enable_edit(e):
- 
-
             task_field.read_only = False
- 
-
             page.update()
- 
-
-
- 
 
         def save_edit(e):
- 
-
-            main_db.update_task_db(task_id, task_field.value)
- 
-
+            main_db.update_task_db(task_id, new_task=task_field.value)
             task_field.read_only = True
- 
-
             page.update()
- 
 
 
- 
 
         return ft.Row([
- 
-
-            task_field, 
- 
-
-            timestamp,
- 
-
-            ft.IconButton(ft.Icons.DELETE, icon_color=ft.Colors.RED_500, on_click=lambda e: delete_task(task_id)),
- 
-
-            ft.IconButton(ft.Icons.EDIT, icon_color=ft.Colors.YELLOW_300, on_click=enable_edit),
- 
-
-            ft.IconButton(ft.Icons.SAVE, icon_color=ft.Colors.GREEN_500, on_click=save_edit)
- 
-
+            task_done,
+            task_field,
+            in_work,
+            ft.IconButton(ft.Icons.EDIT, icon_color=ft.Colors.YELLOW_400, on_click=enable_edit),
+            ft.IconButton(ft.Icons.SAVE, icon_color=ft.Colors.GREEN_400, on_click=save_edit),
+            ft.IconButton(ft.Icons.DELETE, icon_color=ft.Colors.RED_400, on_click=lambda e: delete_task(task_id))
         ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
- 
-
-        
- 
-
-
- 
 
     def add_task(e):
- 
-
         if task_input.value.strip():
- 
-
-            task_id = main_db.add_task_db(task_input.value)
- 
-
-            created_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
- 
-
-            task_list.controls.append(create_task_row(task_id, task_input.value, created_at))
- 
-
-            task_input.value = ''
- 
-
+            task_id = main_db.add_task_db(task_input.value.strip())
+            task_list.controls.append(create_task_row(task_id, task_input.value.strip(), 0))
+            task_input.value = ""
             page.update()
- 
 
+    def toggle_task(task_id, is_completed):
+        status = 2 if is_completed else 0
+        main_db.update_task_db(task_id, completed=status)
+        load_tasks()
 
- 
-
-
- 
+    def toggle_in_work(task_id, is_in_work):
+        if is_in_work:
+            main_db.update_task_status(task_id, 1)
+        else:
+            main_db.update_task_status(task_id, 0) 
+        load_tasks()
 
     def delete_task(task_id):
- 
-
         main_db.delete_task_db(task_id)
- 
-
         load_tasks()
- 
 
+    def set_filter(filter_value):
+        nonlocal filter_type
+        filter_type = filter_value
+        load_tasks()
+
+    def completed_procentage():
+        completed_tasks = main_db.get_tasks("completed")
+        all_tasks = main_db.get_tasks()
+        if all_tasks:
+            return len(completed_tasks) / len(all_tasks) * 100
         
- 
+        return 0
+    
+    def clean_complited(e):
+        main_db.delete_completed_tasks()
+        load_tasks()
 
 
- 
+    task_input = ft.TextField(hint_text='Добавьте задачу', expand=True, dense=True, on_submit=add_task)
+    add_button = ft.ElevatedButton("Добавить", on_click=add_task, icon=ft.Icons.ADD)
 
+    filter_buttons = ft.Row([
+        ft.ElevatedButton("Очистить выполненные", on_click=clean_complited),
+        ft.ElevatedButton("Все", on_click=lambda e: set_filter("all")),
+        ft.ElevatedButton("Выполненные", on_click=lambda e: set_filter("completed")),
+        ft.ElevatedButton("В процессе", on_click=lambda e: set_filter("inwork")),
+        ft.ElevatedButton("Невыполненные", on_click=lambda e: set_filter("incomplete")),
+    ], alignment=ft.MainAxisAlignment.CENTER)
 
- 
-
-    task_input = ft.TextField(label='Add your task', dense=True, expand=True, on_submit=add_task)
- 
-
-    add_button = ft.ElevatedButton('Add', on_click=add_task, icon=ft.icons.ADD)
- 
-
-
- 
-
-    page.add(
- 
-
-        ft.Column([
- 
-
+    content = ft.Container(
+        content=ft.Column([
             ft.Row([task_input, add_button], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
- 
-
+            filter_buttons,
+            ft.Text(f"Процент выполненных задач: {completed_procentage():.2f}%", size=16),
+            ft.Divider(),
             task_list
- 
-
-        ])
- 
-
+        ], alignment=ft.MainAxisAlignment.CENTER),
+        padding=20,
+        alignment=ft.alignment.center
     )
- 
 
+    background_image = ft.Image(
+        src='image.png',
+        fit=ft.ImageFit.FILL,
+        width=page.width,
+        height=page.height
+    )
 
- 
+    background = ft.Stack([background_image, content])
+
+    def on_resize(e):
+        background_image.width = page.width
+        background_image.height = page.height
+        page.update()
+
+    page.on_resize = on_resize
+    page.add(background)
 
     load_tasks()
- 
 
-
- 
 
 if __name__ == '__main__':
- 
-
     main_db.init_db()
- 
-
     ft.app(target=main)
